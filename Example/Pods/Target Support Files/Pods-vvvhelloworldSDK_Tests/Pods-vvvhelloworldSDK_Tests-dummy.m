c121cc5fd3713a4af44abd7973b3213ed593a914
liuyulei <liuyulei@qiyi.com>
2022-01-21 17:13:10 +0800

    firset commit

---
+++ Example/Pods/Target Support Files/Pods-vvvhelloworldSDK_Tests/Pods-vvvhelloworldSDK_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_vvvhelloworldSDK_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_vvvhelloworldSDK_Tests
+@end
---
+++ Example/Pods/Target Support Files/vvvhelloworldSDK/vvvhelloworldSDK-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_vvvhelloworldSDK : NSObject
+@end
+@implementation PodsDummy_vvvhelloworldSDK
+@end
